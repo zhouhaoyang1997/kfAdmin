e51184c381dae0ca41c90bf5cc191ee7360f27b1
zhouhaoyang1997 <zhouhaoyang1997@iCloud.com>
2017-11-19 13:47:14 +0800

    company

---
+++ src/main/java/com/kf/admin/pojo/Company.java
@@ -0,0 +1,15 @@
+package com.kf.admin.pojo;
+
+/**
+ * @author zhy
+ * @create 2017-11-18 21:56
+ **/
+public class Company {
+    Integer companyId;
+    String cpName;
+    String cpContactPerson;
+    String cpAddress;
+    String cp_net;
+    Integer cpSc;
+
+}
